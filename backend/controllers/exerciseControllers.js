215cff2c07cb4725c7d60b3be45b2542accdcf17
Tsegab Bekele <tsegabbekele@gmail.com>
2024-03-16 16:10:01 +0300

    exercise controller added

--- backend/controllers/exerciseControllers.js
+++ backend/controllers/exerciseControllers.js
@@ -1,27 +1,12 @@
 import Exercise from "../models/exercise.model.js";
 import asyncHandler from "express-async-handler";
 
-const getExercise = asyncHandler(async (req, res) => {
-    try {
-        const exercise = await Exercise.find()
-        if (!exercise) {
-            res.status(404)
-            throw new Error('User not found')
-        }
-        res.json(exercise)
-
-    } catch (error) {
-        throw new Error(error)
-    }
-})
-
 const addExercise = asyncHandler(async (req, res) => {
     try {
         const username = req.body.username;
         const description = req.body.description;
         const duration = req.body.duration;
         const date = req.body.date;
-
 
         const exercise = await Exercise.create({
             username,
@@ -47,5 +32,22 @@ const addExercise = asyncHandler(async (req, res) => {
         throw new Error(error)
     }
 })
+
+const getExercise = asyncHandler(async (req, res) => {
+    try {
+        const exercise = await Exercise.find()
+        if (!exercise) {
+            res.status(404)
+            throw new Error('User not found')
+        }
+        res.json(exercise)
+
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
+
+
 
 export { getExercise, addExercise }
--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,9 +1,52 @@
 import express from "express";
 import { getExercise, addExercise } from "../controllers/exerciseControllers.js"
+import Exercise from "../models/exercise.model.js";
 const router = express.Router();
 
 router.route('/').get(getExercise);
 
 router.route('/add').post(addExercise);
+
+router.route('/:id').get((req, res) => {
+
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.status(400).json('Error' + err));
+
+});
+
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Exercise deleted.'))
+        .catch(err => res.status(400).json('Error:' + err))
+});
+
+
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.params.duration);
+            exercise.date = Date.parse(req.body.date);
+
+            exercise.save()
+                .then(() => res.json('Exercise Updated!'))
+                .catch(err => res.status(400).json('Error:' + err));
+        })
+        .catch(err => res.status(400).json('Error:' + err))
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default router
